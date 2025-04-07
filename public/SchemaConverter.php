4fb4606bd08f322fe1091e1d729c804f5ebf09fa
Sumitabha <developers.starpact@gmail.com>
2025-04-07 18:06:20 +0530

    Update schema conversion to use 'orders' table instead of 'products'

--- public/SchemaConverter.php
+++ public/SchemaConverter.php
@@ -128,5 +128,5 @@ class SchemaConverter
 // Example usage:
 
 $converter = new SchemaConverter('localhost', 'bangobasi3', 'root', '');
-$schema = $converter->convertTableToLaravelSchema('products');
+$schema = $converter->convertTableToLaravelSchema('orders');
 echo $schema;
