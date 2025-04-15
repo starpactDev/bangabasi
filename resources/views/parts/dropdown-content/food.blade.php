d8bdbbd4c4864f0b32eb34bd0ab14e214bb095bd
Sumitabha <developers.starpact@gmail.com>
2025-04-15 12:47:28 +0530

    Update food item labels and adjust subcategories in dropdown content

--- resources/views/parts/dropdown-content/food.blade.php
+++ resources/views/parts/dropdown-content/food.blade.php
@@ -2,25 +2,25 @@
     <div class="px-12 py-12 w-full flex flex-wrap f-container">
         <a href="{{ route('user.products', ['category' => 2]) }}" class=" flex flex-col justify-center  w-1/4 px-12 py-8 hover:cursor-pointer border-r">
             <img src="{{ asset('user/uploads/header/' . $headerData['secondRow']['foodItem']['stickerOne']['image']) }}" class="w-10" >
-            Food Items
+            {{ $headerData['secondRow']['foodItem']['stickerOne']['subCategory'] ?? 'Unknown' }}
         </a>
 
         <a href="{{ route('user.products', ['category' => 2]) }}" class=" flex flex-col justify-center  w-1/4 px-12 py-8 hover:cursor-pointer border-r">
             <img src="{{ asset('user/uploads/header/' . $headerData['secondRow']['foodItem']['stickerTwo']['image']) }}" class="w-10" >
 
-            Ekadashi Special
+            {{ $headerData['secondRow']['foodItem']['stickerTwo']['subCategory'] ?? 'Unknown' }}
         </a>
 
         <a href="{{ route('user.products', ['category' => 2]) }}" class=" flex flex-col justify-center  w-1/4 px-12 py-8 hover:cursor-pointer border-r">
             <img src="{{ asset('user/uploads/header/' . $headerData['secondRow']['foodItem']['stickerThree']['image']) }}" class="w-10" >
             
-            Muri
+            {{ $headerData['secondRow']['foodItem']['stickerThree']['subCategory'] ?? 'Unknown' }}
         </a>
 
         <a href="{{ route('user.products', ['category' => 2]) }}" class=" flex flex-col justify-center  w-1/4 px-12 py-8 hover:cursor-pointer ">
             <img src="{{ asset('user/uploads/header/' . $headerData['secondRow']['foodItem']['stickerFour']['image']) }}" class="w-10" >
 
-            Bakery Items
+            {{ $headerData['secondRow']['foodItem']['stickerFour']['subCategory'] ?? 'Unknown' }}
         </a>
 
         
