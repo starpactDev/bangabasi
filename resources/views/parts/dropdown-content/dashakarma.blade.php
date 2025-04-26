a045749cd8ba2af8d53b0d3cc072be18d0e43dc5
Sumitabha <developers.starpact@gmail.com>
2025-04-26 10:15:56 +0530

    Refactor dropdown content structure for improved accessibility and navigation

--- resources/views/parts/dropdown-content/dashakarma.blade.php
+++ resources/views/parts/dropdown-content/dashakarma.blade.php
@@ -1,12 +1,12 @@
 <div class="dropdown-content-sub-1 bg-white">
     <div class="px-12 py-12 w-full flex flex-wrap f-container">
         @foreach($headerData['secondRow']['dashaKarma'] as $sticker)
-            <div class="flex flex-col justify-center items-center w-1/4 px-12 py-6 hover:cursor-pointer border-r">
+            <a href="{{ $sticker['redirect'] }}" class="flex flex-col justify-center items-center w-1/4 px-12 py-6 hover:cursor-pointer border-r">
                 <!-- Dynamic Image -->
                 <img src="{{ asset('user/uploads/header/' . $sticker['image']) }}" class="w-18">
                 <!-- Dynamic Subcategory Name -->
-                <a href="{{ $sticker['redirect'] }}">{{ $sticker['subCategory'] }}</a>
-            </div>
+                <span >{{ $sticker['subCategory'] }}</span>
+            </a>
         @endforeach
     </div>
 </div>
